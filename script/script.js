fec44d34f0901dd1a31503ac72645b4b94db98b9
WritingProject <subliminalnexus@gmail.com>
2016-03-02 09:06:19 -0800

    first function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,12 @@
 $(function(){
+ var test = 0;
  $("button").click(function({
-   $(".title").animate({left: "+=50"},5000);
+   if(test==0){
+    test++;
+     $(".title").animate({marginTop: "500"},10000);
+   },else{
+    test--;
+    $(".title").animate({marginTop: "0"},10000);
+   };
  }));
 });
