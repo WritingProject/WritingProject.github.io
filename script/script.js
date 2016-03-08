71f4b5a5c2b3a0981ed0ee70dc8ffd2be108a4f0
WritingProject <subliminalnexus@gmail.com>
2016-03-08 09:14:21 -0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
+var title = $(".title");
 $(function(){
- var title = $(".title");
  $("button").click(function({
    if(title.css("margin-top") == "0px"){
-    test++;
      title.animate({"margin-top": "500"},10000);
    } else {
-    test--;
     title.animate({"margin-top": "0"},10000);
    };
  }));
